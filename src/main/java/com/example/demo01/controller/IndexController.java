78c047d72b79ae995990080984443b11ee0c4f5f
klose-su <m17760996465@126.com>
2019-11-29 19:33:23 +0800

    2019-11-29

--- src/main/java/com/example/demo01/controller/IndexController.java
+++ src/main/java/com/example/demo01/controller/IndexController.java
@@ -18,8 +18,7 @@ public class IndexController {
     @RequestMapping("/")
     public String index(HttpServletRequest httpServletRequest) {
         Cookie[] cookies = httpServletRequest.getCookies();
-        System.out.println(cookies.length);
-        if (cookies.length == 0) return "index";
+        if (cookies == null) return "index";
         for (Cookie c : cookies) {
             if (c.getName().equals("token")) {
                 String token = c.getValue();
